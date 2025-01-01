8077359c09bbcb9d4f66c250957f8ca6b3a61631
Vladimir Ovechkin <ovchingus@gmail.com>
2025-01-01 17:23:58 +0300

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,9 +1,3 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define MIRYOKU_ALPHAS_QWERTY
-#define MIRYOKU_EXTRA_COLEMAKDH
-#define MIRYOKU_TAP_QWERTY
-#define MIRYOKU_NAV_VI
-#define MIRYOKU_CLIPBOARD_MAC
-
