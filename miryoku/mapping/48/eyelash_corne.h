c40135355b19bc6f847254b63ef9b28c68a76967
plasmadice <19928793+plasmadice@users.noreply.github.com>
2025-06-07 04:02:53 -0400

    experimental build

--- miryoku/mapping/48/eyelash_corne.h
+++ miryoku/mapping/48/eyelash_corne.h
@@ -18,9 +18,9 @@
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04                     XXX                     K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14             XXX     XXX     XXX             K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       XXX                   XXX             K25  K26  K27  K28  K29  XXX \
+K32  K00  K01  K02  K03  K04                     &kp UP                     K05  K06  K07  K08  K09  K37 \
+K33  K10  K11  K12  K13  K14             &kp LEFT     U_BTN1     &kp RIGHT             K15  K16  K17  K18  K19  K36 \
+K34  K20  K21  K22  K23  K24       XXX                   &kp DOWN             K25  K26  K27  K28  K29  K35 \
                     K32  K33  K34                                         K35  K36  K37
 
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 32 33
