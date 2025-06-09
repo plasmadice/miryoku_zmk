5999041ecad86fb147073d839d5913ed66a972e9
plasmadice <19928793+plasmadice@users.noreply.github.com>
2025-06-09 02:33:47 -0400

    Comment out ColemakDH layout definition in custom_config.h for potential future use

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -8,9 +8,9 @@ U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp 
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, SPACE), U_LT(U_FUN, BSPC),  U_NP,              U_NP
 
-// Change colemakdh to qwerty and disable homerow mods on this layer
-#define MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH \
-&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-&kp A,     &kp S,     &kp D,    &kp F,    &kp G,             &kp H,             &kp J,    &kp K,    &kp L,     &kp SQT \
-&kp Z, &kp X,     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,   &kp SLASH,\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, SPACE), U_LT(U_FUN, BSPC),  U_NP,              U_NP
+// // Change colemakdh to qwerty and disable homerow mods on this layer
+// #define MIRYOKU_ALTERNATIVES_BASE_COLEMAKDH \
+// &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+// &kp A,     &kp S,     &kp D,    &kp F,    &kp G,             &kp H,             &kp J,    &kp K,    &kp L,     &kp SQT \
+// &kp Z, &kp X,     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,   &kp SLASH,\
+// U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, SPACE), U_LT(U_FUN, BSPC),  U_NP,              U_NP
