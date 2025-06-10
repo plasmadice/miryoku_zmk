4852c27f5685d7603c5c535c5b942d8fc999a729
plasmadice <19928793+plasmadice@users.noreply.github.com>
2025-06-09 23:54:22 -0400

    Refactor custom_config.h and eyelash_corne.h to remove deprecated game layer definitions, adjust key mappings, and standardize tapping term for improved layout consistency and functionality.

--- miryoku/mapping/48/eyelash_corne.h
+++ miryoku/mapping/48/eyelash_corne.h
@@ -11,9 +11,9 @@
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04                     XXX                     K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14             XXX     XXX     XXX             K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24       XXX                   XXX             K25  K26  K27  K28  K29  XXX \
+K32  K00  K01  K02  K03  K04                     &kp UP                     K05  K06  K07  K08  K09  &u_to_U_BASE \
+&kp LEFT_SHIFT  K10  K11  K12  K13  K14             &kp LEFT     XXX     &kp RIGHT             K15  K16  K17  K18  K19  &kp RIGHT_SHIFT \
+&kp LEFT_GUI  K20  K21  K22  K23  K24       XXX                   &kp DOWN             K25  K26  K27  K28  K29  &kp RIGHT_GUI \
                     K32  K33  K34                                         K35  K36  K37
 
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 32 33
