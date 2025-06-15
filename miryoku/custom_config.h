495473d0df9043bcf0f1813e6d01db67563a7ec2
plasmadice <19928793+plasmadice@users.noreply.github.com>
2025-06-15 01:34:57 -0400

    disable timeless homerow mods, add mute button to encoder, and layer custom config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,6 +1,30 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#if defined (MIRYOKU_KEYBOARD_EYELASH_CORNE)
+
+#define MIRYOKU_LAYERMAPPING_BASE( \
+     K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
+) \
+K32  K00  K01  K02  K03  K04                     &kp UP                     K05  K06  K07  K08  K09  &u_to_U_BASE \
+&kp LEFT_SHIFT  K10  K11  K12  K13  K14             &kp LEFT     &kp F24     &kp RIGHT             K15  K16  K17  K18  K19  &kp RIGHT_SHIFT \
+&kp LEFT_SHIFT  K20  K21  K22  K23  K24       &kp C_MUTE                   &kp DOWN             K25  K26  K27  K28  K29  &kp RIGHT_SHIFT \
+
+#define MIRYOKU_LAYERMAPPING_FUN( \
+     K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
+) \
+K32  K00  K01  K02  K03  K04                     &kp UP                     K05  K06  K07  K08  K09  &u_to_U_BASE \
+&kp LEFT_SHIFT  K10  K11  K12  K13  K14             &kp LEFT     &kp F24     &kp RIGHT             K15  K16  K17  K18  K19  &kp RIGHT_SHIFT \
+&kp LEFT_SHIFT  K20  K21  K22  K23  K24       &kp F5                   &kp DOWN             K25  K26  K27  K28  K29  &kp RIGHT_SHIFT \
+
+#endif
+
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 U_MTL(LGUI, A),     U_MTL(LALT, S),     U_MTL(LCTRL, D),    U_MTL(LSHFT, F),    &kp G,             &kp H,             U_MTR(LSHFT, J),    U_MTR(LCTRL, K),    U_MTR(LALT, L),     U_MTR(LGUI, SQT),   \
--- miryoku/mapping/48/eyelash_corne.h
+++ miryoku/mapping/48/eyelash_corne.h
@@ -11,10 +11,10 @@
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
-K32  K00  K01  K02  K03  K04                     &kp UP                     K05  K06  K07  K08  K09  &u_to_U_BASE \
-&kp LEFT_SHIFT  K10  K11  K12  K13  K14             &kp LEFT     XXX     &kp RIGHT             K15  K16  K17  K18  K19  &kp RIGHT_SHIFT \
-&kp LEFT_SHIFT  K20  K21  K22  K23  K24       XXX                   &kp DOWN             K25  K26  K27  K28  K29  &kp RIGHT_SHIFT \
-                    K32  K33  K34                                         K35  K36  K37
+XXX  K00  K01  K02  K03  K04                     &kp UP                     K05  K06  K07  K08  K09  XXX \
+XXX  K10  K11  K12  K13  K14             &kp LEFT  XXX  &kp RIGHT           K15  K16  K17  K18  K19  XXX \
+XXX  K20  K21  K22  K23  K24  &kp C_MUTE      &kp DOWN                      K25  K26  K27  K28  K29  XXX \
+                    K32  K33  K34                                           K35  K36  K37
 
 #endif
 
