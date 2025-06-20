82bbd352eb0dddb5fa67fbb9689090b8aaadf6bd
plasmadice <19928793+plasmadice@users.noreply.github.com>
2025-06-20 01:44:31 -0400

    Refactor custom_config.h and eyelash_corne.h to remove unused layer mappings and simplify key definitions

--- miryoku/mapping/48/eyelash_corne.h
+++ miryoku/mapping/48/eyelash_corne.h
@@ -5,17 +5,19 @@
 
 #define XXX &none
 
+#define LSH &kp LEFT_SHIFT
+#define RSH &kp RIGHT_SHIFT
+
 #define MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE( \
      K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04                     &kp UP                     K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14             &kp LEFT  XXX  &kp RIGHT           K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24  &kp C_MUTE      &kp DOWN                      K25  K26  K27  K28  K29  XXX \
-                    K32  K33  K34                                           K35  K36  K37
-
+K32  K00  K01  K02  K03  K04                     &kp UP                K05  K06  K07  K08  K09  K37 \
+K32  K10  K11  K12  K13  K14             &kp LEFT  &u_to_U_BASE  &kp RIGHT  K15  K16  K17  K18  K19  K35 \
+LSH  K20  K21  K22  K23  K24  &kp C_MUTE         &kp DOWN              K25  K26  K27  K28  K29  RSH \
+                    K32  K33  K34                                 K35  K36  K37
 #endif
 
 #define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_EYELASH_CORNE 
