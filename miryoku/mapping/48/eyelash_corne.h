54eac1430c1f0d0fc6cd1c8ecc8645ae6fbf3101
plasmadice <19928793+plasmadice@users.noreply.github.com>
2025-06-20 13:34:15 -0400

    map modifiers to outer columns

--- miryoku/mapping/48/eyelash_corne.h
+++ miryoku/mapping/48/eyelash_corne.h
@@ -8,6 +8,8 @@
 #define LSH &kp LEFT_SHIFT
 #define RSH &kp RIGHT_SHIFT
 #define LGU &kp LEFT_GUI
+#define LAL &kp LEFT_ALT
+#define RAL &kp RIGHT_ALT
 #define RGU &kp RIGHT_GUI
 #define MTE &kp C_MUTE
 
@@ -17,7 +19,7 @@
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
-K32  K00  K01  K02  K03  K04                &kp UP                     K05  K06  K07  K08  K09  K37 \
+LAL  K00  K01  K02  K03  K04                &kp UP                     K05  K06  K07  K08  K09  RAL \
 LGU  K10  K11  K12  K13  K14    &kp LEFT    &u_to_U_BASE  &kp RIGHT    K15  K16  K17  K18  K19  RGU \
 LSH  K20  K21  K22  K23  K24    MTE         &kp DOWN                   K25  K26  K27  K28  K29  RSH \
                K32  K33  K34                                           K35  K36  K37
