b00539f9662bf1042e53d3e9026807a745f1abc3
plasmadice <19928793+plasmadice@users.noreply.github.com>
2025-06-20 01:17:10 -0400

    fix(build): roll back, switch to bongo cat, and enhance custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,6 +1,11 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_CLIPBOARD_MAC
+#define MIRYOKU_NAV_VI
+
 #define MIRYOKU_KEYBOARD_EYELASH_CORNE
 
 #if defined (MIRYOKU_KEYBOARD_EYELASH_CORNE)
@@ -11,8 +16,8 @@
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
-K32  K00  K01  K02  K03  K04                     &kp UP                     K05  K06  K07  K08  K09  &u_to_U_BASE \
-&kp LEFT_SHIFT  K10  K11  K12  K13  K14             &kp LEFT     &kp F24     &kp RIGHT             K15  K16  K17  K18  K19  &kp RIGHT_SHIFT \
+K32  K00  K01  K02  K03  K04                     &kp UP                     K05  K06  K07  K08  K09  K37 \
+K32  K10  K11  K12  K13  K14             &kp LEFT     &kp F24     &kp RIGHT             K15  K16  K17  K18  K19  K35 \
 &kp LEFT_SHIFT  K20  K21  K22  K23  K24       &kp C_MUTE                   &kp DOWN             K25  K26  K27  K28  K29  &kp RIGHT_SHIFT \
                     K32  K33  K34                                           K35  K36  K37
 
